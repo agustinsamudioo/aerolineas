33562f04a59e4c9794cac5ad3c9d4963db2adeaa
AgustinSamudioJS <agustinsamudio95@gmail.com>
2024-10-27 15:55:42 -0300

    actualizacion

--- aerolineaBondiJet/src/aerolineaBondiJet/Vuelo.java
+++ aerolineaBondiJet/src/aerolineaBondiJet/Vuelo.java
@@ -1,7 +1,10 @@
 package aerolineaBondiJet;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.lang.model.element.NestingKind;
 
 public class Vuelo {
 int id_vuelo;
@@ -11,11 +14,23 @@ int capacidad;
 Date hora_salida;
 Date hora_llegada;
 int cant_pasajeros;
-Date fecha_vuelo;
+int cant_tripulantes;
+String fecha_salida_vuelo;
 Aeropuerto aeropuerto_llegada;
 HashMap<Integer, Pasajero>pasajeros;
+Refrigerio refrigerio;
+int[] cantAsientos;
+Double[]precios;
 
-public Vuelo() {
-	
+
+public Vuelo(String origen, String destino, String fecha, int tripulantes, Double valorRefrigerio,
+		Double []precios, int[]cantAsientos) {
+	this.aeropuerto_salida.nombre=origen;
+	this.aeropuerto_salida.nombre=destino;
+	this.fecha_salida_vuelo=fecha;
+	this.cant_tripulantes=tripulantes;
+	this.refrigerio.precio=valorRefrigerio;
+	this.precios=precios;
+	this.cantAsientos=cantAsientos;
 }
 }
