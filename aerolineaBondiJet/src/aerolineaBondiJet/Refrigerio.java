e4e16eec5d62dda09a2e08bdf118b4e5b0ca6a05
AgustinSamudioJS <agustinsamudio95@gmail.com>
2024-10-27 15:32:23 -0300

    actualizacion

--- aerolineaBondiJet/src/aerolineaBondiJet/Refrigerio.java
+++ aerolineaBondiJet/src/aerolineaBondiJet/Refrigerio.java
@@ -1,5 +1,10 @@
 package aerolineaBondiJet;
 
 public class Refrigerio {
-
+int cantidad;
+Double precio;
+public Refrigerio(int cantidad, Double precio) {
+	this.cantidad=cantidad;
+	this.precio=precio;
+}
 }
