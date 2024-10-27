1a1f5416bb75c78984706f188fb0b8e10e716262
AgustinSamudioJS <agustinsamudio95@gmail.com>
2024-10-27 15:59:11 -0300

    .

--- aerolineaBondiJet/src/aerolineaBondiJet/VueloInternacional.java
+++ aerolineaBondiJet/src/aerolineaBondiJet/VueloInternacional.java
@@ -2,4 +2,10 @@ package aerolineaBondiJet;
 
 public class VueloInternacional extends Vuelo {
 
+	public VueloInternacional(String origen, String destino, String fecha, int tripulantes, Double valorRefrigerio,
+			Double[] precios, int[] cantAsientos) {
+		super(origen, destino, fecha, tripulantes, valorRefrigerio, precios, cantAsientos);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- aerolineaBondiJet/src/aerolineaBondiJet/VueloNacional.java
+++ aerolineaBondiJet/src/aerolineaBondiJet/VueloNacional.java
@@ -3,7 +3,10 @@ package aerolineaBondiJet;
 public class VueloNacional extends Vuelo{
 int cant_secciones;
 int max_pasajeros;
-public VueloNacional() {
-	
+public VueloNacional(String origen, String destino, String fecha, int tripulantes, Double valorRefrigerio,
+		Double[] precios, int[] cantAsientos) {
+	super(origen, destino, fecha, tripulantes, valorRefrigerio, precios, cantAsientos);
+
 }
+
 }
